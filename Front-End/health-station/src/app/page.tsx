a9aab04421d196e8e7d0c22148e79cd547ac4b17
Julio Felipe <julio.f4costa@hotmail.com>
2024-05-13 01:54:23 -0300

    started create front-end

---
+++ Front-End/health-station/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Button } from 'primereact/button';                             
+
+export default function Home() {
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
